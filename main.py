da885a138e2a9e09d3c99e80939e2718bc1c3d16
cacko <alex@cacko.net>
2021-08-27 16:36:21 +0100

    trying to do something right!

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from PyQt6.QtGui import QIcon
 from gui import App
 from PyQt6.QtWidgets import QApplication
 import sys
@@ -6,6 +7,9 @@ if __name__ == "__main__":
     try:
         app = QApplication(sys.argv)
         app.setStyle("Fusion")
+        icon = QIcon()
+        icon.addFile('capy.png')
+        app.setWindowIcon(icon)
         a = App()
         a.show()
         sys.exit(app.exec())
